7a6b38b4dd71b7bfeae05c93f0950111e0d4f5a9
ManviSharma09 <manvis@geekyants.com>
2020-08-11 00:25:03 +0530

    done the boilerplate setup

---
+++ src/routes/ProfileDashboard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const DashBoardContainer = styled.div`
+  height: 100vh;
+`;
+
+const ProfileDashboard = () => {
+  return <DashBoardContainer>This is profile Dashboard</DashBoardContainer>;
+};
+
+export default ProfileDashboard;
